26f94d8614c657b2b0212d8a24526746ae370480
sampada payal <sampadapayal8@gmail.com>
2024-01-18 19:02:52 +0530

    expressjs

--- server.js
+++ server.js
@@ -10,9 +10,15 @@ app.use(express.json());
 
 const port = 5500;
 
+//ARRAY
 const toDoList = ["hey everyone!", "hope you", "all are", "doing good :)"];
 
 app.get("/todos", (req, res) => {
     //CALLBACK
     res.status(200).send(toDoList)
 });
+
+//server listening
+app.listen(port, () => {
+    console.log(`NodeJs with Express started running successfully on port ${port} `)
+})
